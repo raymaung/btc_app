c06a820b23a5783665972b0315aac57df2323045
Raymond Maung <raymaung@gmail.com>
2019-02-02 10:55:37 +1100

    Add deribit

---
+++ apps/deribit/lib/deribit.ex
@@ -0,0 +1,18 @@
+defmodule Deribit do
+  @moduledoc """
+  Documentation for Deribit.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Deribit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
